159f97e5b61b57d5fa40c74ed528bb6009dae435
wy <7758521@qq.com>
2022-05-13 21:27:23 +0800

    master commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,12 +1,7 @@
 public class gitTest {
     public static void main(String[] args) {
 
-        System.out.println("hello git1");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("hello git5");
-        System.out.println("hello git6");
-        System.out.println("master test!");
+        System.out.println("hello atguigu");
+        System.out.println("unbelievable");
     }
 }
