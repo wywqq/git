db1055bb25e22696c676374c8d9084430ccd6ee0
wy <7758521@qq.com>
2022-05-13 21:21:52 +0800

    second commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,2 +1,12 @@
 public class gitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+        System.out.println("hello git6");
+        System.out.println("master test!");
+    }
 }
